cfd62526bf8bcb2ab0c9c0dbb943c6ecf6411884
ChoiMoonSeok <mon08290@naver.com>
2022-12-09 21:25:12 +0900

    파일 삭제

--- CarbonServerDjango/db_config.py
+++
@@ -1,8 +0,0 @@
-DATABASES = {
-    'default' : {
-        'ENGINE': 'django.db.backends.mysql',    
-        'NAME': 'carbon_project',                  
-        'USER': 'root',                          
-        'PASSWORD': 'carbon1028',                         
-    }
-}
--- CarbonServerDjango/setDjango.sh
+++
@@ -1,8 +0,0 @@
-conda activate tf
-cd
-cd venv/DjangoENV/bin
-source activate
-cd
-cd projects/CarbonServerDjango
-
-export PATH="$PATH:/usr/local/mysql/bin"
